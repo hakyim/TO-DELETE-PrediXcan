5f6ab413c9c6c5f8c07f5b7c86487b95d1d895b6
hriordan <mightofh@gmail.com>
2014-09-19 22:55:03 -0500

    updated parsing script for the LASSO

--- parsebeta.py
+++ parsebeta.py
@@ -4,15 +4,22 @@ import MySQLdb as db
 import pandas as pd
 
 #ABC11-48400900C8.1-WB.GTEx.txt
-def parse_title(filename):
-	step1 = filename.rsplit("-",1)
-	#print step1
-	gene = step1[0]
-	step2 = step1[1].split(".")
-	#print step2
-	tissue = step2[0]
-	study = step2[1]
-	return (gene, tissue, study)
+def parse_title(filename,type='regular'):
+        if type == 'regular':
+                step1 = filename.rsplit("-",1)
+                #print step1
+                gene = step1[0]
+                step2 = step1[1].split(".")
+                #print step2
+                tissue = step2[0]
+                study = step2[1]
+                return (gene, tissue, study)
+        elif type == 'LASSO':
+                step1 = filename.split('-')
+                gene = step1[0]
+                study = step1[1]
+                tissue = step1[2].split('.')[0] #parameter gore 
+                return (gene, tissue, study) 
 
 ## MAIN ## 
 
@@ -20,10 +27,12 @@ def parse_title(filename):
 parser = argparse.ArgumentParser(description="Parse input/output files.")
 parser.add_argument("--betapath", help="path for betafiles")
 parser.add_argument("--filelist", help="list of beta-files to process", default="trfilelist.txt")
+parser.add_argument("--filetype", help="format of beta files: regular vs LASSO",default='regular')
 args = parser.parse_args()
 
 fl = args.filelist 
 pathname = args.betapath
+filetype = args.filetype 
 
 filelist = open(fl)
 
@@ -33,20 +42,20 @@ database = db.connect(host="localhost", # your host
                       db="mysql") # name of the data base
 cur = database.cursor()
 
-
+print filetype
 for fname in filelist.readlines():
 	fname = fname.strip('\n')
 	fullpath = pathname + fname
-	gene,tissue,study = parse_title(fname)
+	gene,tissue,study = parse_title(fname,filetype)
 	try:
 		snpframe = pd.read_table(fullpath)
 	except:
 		print "error in read_table", sys.exc_info()[0]
 		print fullpath
 		continue 
-        
-        #fuck. need to account for missing N. DAMN IT.
-        if snpframe.cis.isnull()[0]:
+      
+
+        if snpframe.cis.isnull()[0] and filetype=='regular':
                 snpframe.cis = snpframe.N
                 snpframe.N = snpframe.N.map(lambda x: np.nan if x == True else x)
                 snpframe = snpframe.where(pd.notnull(snpframe), None)
@@ -61,20 +70,30 @@ for fname in filelist.readlines():
                         
                 if nullparts == True:
                         numparts = "Null"
-                else:
+                elif filetype=='regular':
                         numparts = row[1]["N"]
 
-                statement = """INSERT INTO SNPs (rsnum, eff_allele, beta, p_value, N, cis, genename, tissue, study_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
-                 
-                try:
-                        cur.execute(statement,(row[1]["SNP"],row[1]["eff.allele"],row[1]["beta"],row[1]["p.value"],numparts,row[1]["cis"],gene,tissue,study) )
+                statement = """INSERT INTO SNPs (rsnum, eff_allele, beta, p_value, N, cis, genename, tissue, study_name, version) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+                
+                if filetype=='regular':
+                        try:
+                                cur.execute(statement,(row[1]["SNP"],row[1]["eff.allele"],row[1]["beta"],row[1]["p.value"],numparts,row[1]["cis"],gene,tissue,study) )
+                                
+                        except:
+                                err = sys.exc_info()[0]
+                                print "Error: %s" % err
+                                print "On file: %s" % fname 
+                                continue
+                elif filetype=='LASSO':
+                        try:
+                                cur.execute(statement,(row[1]["SNP"],row[1]["eff.allele"],row[1]["beta"],"Null","Null","Null",gene,tissue,study) )
                         
-                except:
-                        err = sys.exc_info()[0]
-                        print "Error: %s" % err
-                        print "On file: %s" % fname 
-                        continue
-                       
+                        except:
+                                err = sys.exc_info()[0]
+                                print "Error: %s" % err
+                                print "On file: %s" % fname 
+                                continue
+                        
                 database.commit()
                 """
                 cur.execute("SELECT * FROM SNPs where genename = %s;", gene)
