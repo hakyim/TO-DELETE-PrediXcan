4f25ab7a3525e366f77be2d92588c2f6d3609258
hriordan <mightofh@gmail.com>
2014-12-11 16:47:32 -0600

    uptodate?

--- predflaskapp/app/__init__.py
+++ predflaskapp/app/__init__.py
@@ -1,8 +1,17 @@
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 from flask import Flask
+from config import basedir
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+
+oid = OpenID(app, os.path.join(basedir,'tmp'))
 from app import views, models
--- predflaskapp/app/views.py
+++ predflaskapp/app/views.py
@@ -1,20 +1,54 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from models import User
 
 @app.route('/login',methods=['GET','POST'])
+@oid.loginhandler
 def login():
+	if g.user is not None and g.user.is_authenticated():
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
+		session['remember_me'] = form.remember_me.data
+		return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+		"""
 		flash('Login requested for jankyID="%s", remember_me=%s' %	
 			(form.openid.data, str(form.remember_me.data)))
 		return redirect('/index') 
+		"""
 	return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+	if resp.email is None or resp.email == "":
+		flash('invalid login. please try again.')
+		return redirect(url_for('login'))
+	user = User.query.filter_by(email=resp.email).first()
+	if user is None:
+		nickname = resp.nickname
+		if nickname is None or nickname == "":
+			nickname = resp.email.split('@')[0]
+		user = User(nickname=nickname,email=resp.email)
+		db.session.add(user)
+		db.session.commit()
+	remember_me = False
+	if 'remember_me' in session:
+		remember_me = session['remember_me']
+		session.pop('remember_me',None)
+	login_user(user, remember = remember_me)
+	return redirect(request.args.get('next') or url_for('index'))
+
+@app.before_request
+def before_request():
+	g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Henry'}  
+    user = g.user
     posts = [ 
 	    {
 	    	'author':{'nickname':'John'},
@@ -29,6 +63,28 @@ def index():
     return render_template('index.html',
     						title='home',
     						user=user,
-    						posts=posts)		
+    						posts=posts)
 
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
 
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+"""here lie user profiels"""
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+	user = User.query.filter_by(nickname=nickname).first()
+	if user == None: 
+		flash('user %s not found.' % nickname)
+		return redirect(url_for('index'))
+	posts = [
+		{'author': user, 'body': 'test 1'},
+		{'author': user, 'body': 'test 2'}
+	]
+	return render_template('user.html', user=user, posts=posts)
+	
