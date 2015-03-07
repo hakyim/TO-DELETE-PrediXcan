411fe64a0a610daa2dd5a027e94253330064674b
Henry Riordan <mightofh@gmail.com>
2015-03-06 18:11:01 -0600

    adding in tornado wsgi

--- predflaskapp/run.py
+++ predflaskapp/run.py
@@ -3,6 +3,10 @@ from app import app
 from OpenSSL import SSL
 import ssl
 import traceback
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+import os
 
 """
 context = SSL.Context(SSL.TLSv1_METHOD)
@@ -11,6 +15,12 @@ context.use_certificate_file('server.crt')
 """
 
 context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-
 context.load_cert_chain('server.crt','server.key')
 app.run(debug=True,ssl_context=context,port=443,host='0.0.0.0')
+
+
+settings = { "certfile": os.path.join("server.crt"), "keyfile": os.path.join("server.key") }
+
+http_server = HTTPServer(WSGIContainer(app),ssl_options=settings)
+http_server.listen(5000)
+IOLoop.instance().start()
