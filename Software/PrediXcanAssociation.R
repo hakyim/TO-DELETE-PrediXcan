2acb609c223d485bfc3f4386d68bd51366664762
Scott Dickinson <spdickinson88@gmail.com>
2016-01-12 12:03:46 -0600

    built out many functions of PrediXcanAssociation.R
    Modified read, merge_and_filter, and association.  Got rid of some
    other functions to make sure merging was done appropriately.

--- Software/PrediXcanAssociation.R
+++ Software/PrediXcanAssociation.R
@@ -1,48 +1,75 @@
 
-read_pheno <- function(pheno_file) {
-    pheno <- read.table(pheno_file, as.is=TRUE)
-    return(pheno)
+read_pheno <- function(pheno_file, pheno_column = NULL) {
+  pheno <- read.table(pheno_file, as.is = T)
+  # Only keep first 2 columns and the phenotype column we care about.
+  # If user does not specify pheno_column, phenotype vals are in last column.
+  if (pheno_column != NULL) {
+    pheno <- pheno(c(1, 2, pheno_column))
+  } else {
+    pheno <- pheno(c(1, 2, ncol(pheno)))
+  }
+  names(pheno) <- c("fid", "iid", "phenotype")
+  return(pheno)
 }
 
-read_filter <- function(filter_file, filter_column) {
-    fil <- read.table(filter_file, as.is=TRUE)
-    # Only keep columns of filter file necessary
-    fil <- fil[c(1, 2, filter_column)]
-    names(fil) <- c("FID", "IID", "FIL_VAL")
-    return(fil)
+read_filter <- function(filter_file, filter_column = 3) {
+  fil <- read.table(filter_file, as.is = T)
+  # Only keep columns of filter file necessary
+  fil <- fil[c(1, 2, filter_column)]
+  names(fil) <- c("fid", "iid", "fil_val")
+  return(fil)
 }
     
 read_predicted <- function(pred_exp_file) {
-    pred_exp <- read.delim(pred_exp_file)
-    return(pred_exp)
+  pred_exp <- read.delim(pred_exp_file)
+  return(pred_exp)
 }
 
-# Filter out rows of the phenotype dataframe
-filter_pheno <- function(pheno, fil, filter_on) {
-    # merge the pheno df and fil by the Family ID and Individul ID columns
-    pheno <- merge(pheno, fil, by=c(1,2), sort=FALSE)
-    pheno <- filter(pheno, FIL_VAL==filter_on)
-    pheno$FIL_VAL <- NULL
-    return(pheno)
+merge_and_filter <- function(pheno, pred_exp, fil = NULL, filter_val = 1) {
+  # Column binds the phenotype and predicted expression dataframes and
+  # filters them based on the optionally provided filter file (fil)
+  # 
+  # IMPORTANT: Each row in the pheno and pred_exp dfs represent people so
+  # for the cbind to make sense, the row numbers in each data frame must
+  # correspond to the same person.
+  if (fil != NULL) {
+    merged <- merge(cbind(pheno, pred_exp), fil, by = c(1, 2), sort = F)
+    merged <- filter(merged, FIL_VAL = filter_val)
+  } else {
+    merged <- cbind(pheno, pred_exp)
+  }
+  return(merged)
 }
 
-association <- function(pheno, pred_exp, fil, filter_on) {
-    genes <- colnames(pred_exp)
-    merged <- merge(cbind(pheno, pred_exp), fil, by=c(1,2), sort=FALSE)
-    filtered <- filter(merged, FIL_VAL=filter_on)
+association <- function(merged, genes, test_type = "logistic") {
+  assoc_df <- NULL
 
-    pheno <- filtered$PHENO
-    filtered <- subset(filtered, select = genes)
-
-    OUT<-NULL
-    for (gene in genes) {
-      data <- filtered[[gene]]
-      results <- coef(summary(lm(pheno ~ data)))[c(2,6,8,4)]
-      line <- c(gene,results)
-      OUT <- rbind(OUT,line)
+  for (gene in genes) {
+    pred_gene_exp <- merged[[gene]]
+    # TODO: Remove na's in phenotype, and if doing a logistic regression,
+    # make sure values 0 or 1.
+    if (test_type == "logistic") { 
+      model <- glm(phenotype ~ pred_gene_exp, data = merged, family = binomial)
+    } else if (test_type == "linear") {
+      model <- lm(phenotype ~ pred_gene_exp, data = merged)
+    } else if (test_type == "survival") {
+      # TODO
+      model <- NULL
     }
-    colnames(OUT) <- c("gene","beta","z-stat","p-val", "se(beta)")
-    return(OUT)
+    results <- coef(summary(model))[c(2,6,8,4)]
+    line <- c(gene,results)
+    assoc_df <- rbind(assoc_df,line)
+  }
+
+  # Specify column names----
+  if (test_type == "logistic") {
+    colnames(assoc_df) <- c("gene", "beta", "z", "p", "se(beta)")
+  } else if (test_type == "linear") {
+    colnames(assoc_df) <- c("gene", "beta", "t", "p", "se(beta)")
+  } else if (test_type == "survival") {
+    # TODO
+  }
+  return(assoc_df)
 }
 
 write_association <- function(OUT, output_file) {
