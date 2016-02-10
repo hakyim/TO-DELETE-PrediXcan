574d49c97d16563b33baa4657e3b63347184fe9f
Scott Dickinson <spdickinson88@gmail.com>
2016-02-10 09:34:01 -0600

    Pull Request with Flipped Allele

--- Software/PrediXcan.py
+++ Software/PrediXcan.py
@@ -98,9 +98,12 @@ class TranscriptionMatrix:
             self.gene_list = self.get_gene_list()
             self.gene_index = { gene:k for (k, gene) in enumerate(self.gene_list) }
             self.D = np.zeros((len(self.gene_list), len(dosage_row))) # Genes x Cases
-        if gene in self.gene_index:
-            multiplier = 1 if ref_allele == allele else -1
-            self.D[self.gene_index[gene],] += dosage_row * weight * multiplier # Update all cases for that gene
+        if gene in self.gene_index: #assumes strands are aligned to PrediXcan reference and dosage coding 0 to 2           
+            if ref_allele == allele: 
+                self.D[self.gene_index[gene],] += dosage_row * weight
+            else:
+                self.D[self.gene_index[gene],] += (2-dosage_row) * weight # Update all cases for that gene 
+
 
     def get_samples(self):
         with open(self.sample_file, 'r') as samples:
